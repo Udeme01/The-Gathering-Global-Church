ffb3168cd770421745152486579c5b1ee97e866f
Udeme01 <emmanueluddy2@gmail.com>
2026-01-23 23:38:06 +0100

    feat: added social handles to mobile nav

--- src/components/layout/MobileNav.jsx
+++ src/components/layout/MobileNav.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect } from "react";
 import { navigations } from "../../data/navigation";
 import { NavLink } from "react-router";
+import { socialHandles } from "../../data/socialsHandles";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const MobileNav = ({ isOpen }) => {
   useEffect(() => {
@@ -26,7 +28,7 @@ const MobileNav = ({ isOpen }) => {
         style={{ zIndex: 39 }}
       /> */}
       <section
-        className={`lg:hidden bg-black text-white fixed top-0 bottom-0 left-0 right-0 transition-transform duration-500 ease-in-out z-40 ${isOpen ? "translate-x-0" : "translate-x-full"}`}
+        className={`lg:hidden bg-black text-white fixed top-0 bottom-0 left-0 right-0 transition-transform duration-500 ease-in-out z-40 overflow-y-auto ${isOpen ? "translate-x-0" : "translate-x-full"}`}
       >
         <section className="w-[90%] sm:w-[85%] mx-auto">
           <div className="py-8.5 w-fit ">
@@ -36,7 +38,7 @@ const MobileNav = ({ isOpen }) => {
             />
           </div>
           {/* mobile navigation */}
-          <nav className="min-h-screen mt-10">
+          <nav className="mt-10">
             <ul className="text-3xl flex flex-col gap-6">
               {navigations.map((navigation) => {
                 const { anchorName, anchorLink } = navigation;
@@ -60,6 +62,51 @@ const MobileNav = ({ isOpen }) => {
               })}
             </ul>
           </nav>
+          {/* join us */}
+          <section className="mt-10 pb-8 bg-linear-to-br from-purple-900 via-purple-950 to-black rounded-2xl py-8 px-4 shadow-2xl border border-purple-800/30">
+            <h1
+              className="text-6xl font-bold text-center px-3 mb-2"
+              style={{
+                WebkitTextStroke: "2px white",
+                WebkitTextFillColor: "transparent",
+                paintOrder: "stroke fill",
+              }}
+            >
+              Join Us Every
+            </h1>
+
+            <div className="flex mt-10 justify-around px-4">
+              <p className="text-2xl font-bold">
+                Sunday: <span className="text-purple-300">9AM</span>
+              </p>
+              <p className="text-2xl font-bold">
+                Thursday: <span className="text-purple-300">6PM</span>
+              </p>
+            </div>
+          </section>
+
+          {/* socials */}
+          <section className="mt-10 pb-8">
+            <h2 className="text-xl font-semibold mb-4 text-purple-300">
+              Connect With Us
+            </h2>
+            <div className="flex gap-2 justify-center">
+              {socialHandles.map((socialHandle) => {
+                return (
+                  <a
+                    key={socialHandle.name}
+                    href={socialHandle.href}
+                    target={socialHandle.target}
+                    rel={socialHandle.rel}
+                    className="bg-purple-900/50 hover:bg-purple-800 rounded-full transition-colors p-3"
+                    aria-label={socialHandle.ariaLabel}
+                  >
+                    <FontAwesomeIcon icon={socialHandle.icon} size="lg" />
+                  </a>
+                );
+              })}
+            </div>
+          </section>
         </section>
       </section>
     </>
---
+++ src/data/socialsHandles.js
@@ -0,0 +1,69 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTiktok,
+  faYoutube,
+  faTelegram,
+  faSpotify,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+
+export const socialHandles = [
+  {
+    icon: faFacebook,
+    name: "Facebook",
+    href: "https://web.facebook.com/thegatheringglobe",
+    target: "_blank",
+    rel: "noopener noreferrer",
+    ariaLabel: "Facebook",
+  },
+  {
+    icon: faInstagram,
+    name: "Instagram",
+    href: "https://www.instagram.com/thegathering_global/",
+    target: "_blank",
+    rel: "noopener noreferrer",
+    ariaLabel: "Instagram",
+  },
+  {
+    icon: faTiktok,
+    name: "Tiktok",
+    href: "https://www.tiktok.com/@the.gathering.glo",
+    target: "_blank",
+    rel: "noopener noreferrer",
+    ariaLabel: "Tiktok",
+  },
+  {
+    icon: faYoutube,
+    name: "YouTube",
+    href: "https://www.youtube.com/@thegatheringglobalchurch",
+    target: "_blank",
+    rel: "noopener noreferrer",
+    ariaLabel: "YouTube",
+  },
+  {
+    icon: faTelegram,
+    name: "Telegram",
+    href: "https://t.me/TGGlobalChurch",
+    target: "_blank",
+    rel: "noopener noreferrer",
+    ariaLabel: "Telegram",
+  },
+  {
+    icon: faSpotify,
+    name: "Spotify",
+    href: "https://open.spotify.com/show/3vM86TcfGSU64mSuOvp2DN?si=XfeL8kLES86yxYYXbglNvA",
+    target: "_blank",
+    rel: "noopener noreferrer",
+    ariaLabel: "Spotify",
+  },
+  {
+    icon: faWhatsapp,
+    name: "WhatsApp",
+    href: "https://whatsapp.com/channel/0029Vb34fXdFsn0krP786s2b",
+    target: "_blank",
+    rel: "noopener noreferrer",
+    ariaLabel: "WhatsApp",
+  },
+];
